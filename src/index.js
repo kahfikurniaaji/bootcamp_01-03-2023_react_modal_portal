c4dbbe86ab2205ed3a3f1d5491fb458331e282ff
Kahfi Kurnia Aji <58323098+kahfikurniaaji@users.noreply.github.com>
2023-02-28 11:44:40 +0700

    React Final Form

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 // Import module yang diperlukan
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import App from "./component/App";
-import reducers from "./redux/reducers/combine-reducers";
+import ReactDOM from "react-dom";
+import App from "./component/App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// Membuat store baru
-const store = createStore(reducers, applyMiddleware(thunk));
-
 // Merender App
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+root.render(<App />);
